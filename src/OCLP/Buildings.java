6f7004c66e6f2cfd2b5cb95553bd4d142092688b
Kevin <vosssjoebeck@hotmail.com>
2012-06-16 12:21:08 +0200

    Lots of updates
    Fogs works again, and the buildings gets automatically deleted after
    they reached -10 on the x-coordinate

--- src/OCLP/Buildings.java
+++ src/OCLP/Buildings.java
@@ -1,11 +1,13 @@
 package OCLP;
 
+import OCLP.Controls.BuildingControl;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
 import com.jme3.scene.Node;
 import com.jme3.math.Vector3f;
 import com.jme3.asset.*;
 import com.jme3.math.Vector2f;
+import com.jme3.scene.Spatial;
 
 
 /**
@@ -16,7 +18,8 @@ public class Buildings{
     private AssetManager assetManager;
     private Node rootNode;
     
-    private Node Building1;
+    private Node buildings;
+    private int buildingNR = 0;
     
     public Buildings(AssetManager assetManager, Node rootNode){
         //Give access to assetManager for other functions to use it in this class
@@ -27,37 +30,37 @@ public class Buildings{
         SpawnBuildings();
     }
     
-    public void SpawnBuildings(){
-        Building1 = new Node();
+    private void SpawnBuildings(){
+        buildings = new Node();
         
         //Spawn buildings
-        Geometry geometry_building = createBuilding();
+        Spatial geometry_building = createBuilding();
+        geometry_building.setUserData("isAlive", true);
+        geometry_building.addControl(new BuildingControl());
         
-        Building1.attachChild(geometry_building);
+        buildings.attachChild(geometry_building);
         
-        rootNode.attachChild(Building1);
+        rootNode.attachChild(buildings);
     }
     
     private Geometry createBuilding(){
         Vector3f boxVector = new Vector3f(9, 0.9f, 0.2f);
         Box building = new Box(boxVector, 0.2f, 3f, 0.2f);
         building.scaleTextureCoordinates(new Vector2f(1f,10f));
-        Geometry geometry_building = new Geometry("Building", building);
+        Geometry geometry_building = new Geometry("Building" + buildingNR, building);
         geometry_building.setLocalTranslation(0f,2.05f,0f);
         geometry_building.setMaterial(assetManager.loadMaterial("Materials/Building.j3m"));
 
         return geometry_building;
     }
-    
-    /* Other actions */
-    public void tickPosition(){
-      //Is building1 past camera?
-      Vector3f Building1Pos = Building1.getWorldTranslation();
-      System.out.print(Building1Pos.x+"\n");
-      if(Building1Pos.x < -14){
-          Building1.setLocalTranslation(0, 0, 0);
-      }
-      Building1.move(-0.008f, 0, 0);
-        
+
+    void checkOutOfBounds() {
+        for(Spatial s: buildings.getChildren()){
+            boolean data = (Boolean)s.getUserData("isAlive");           
+            if(data == false){
+                buildings.detachChild(s);
+            }           
+        }
     }
+   
 }
--- src/OCLP/Main.java
+++ src/OCLP/Main.java
@@ -1,15 +1,10 @@
 package OCLP;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
-import com.jme3.math.Vector3f;
-import com.jme3.renderer.RenderManager;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Box;
 
 
 public class Main extends SimpleApplication {
+    
     Buildings buildings;
     
     public static void main(String[] args) {
@@ -20,23 +15,22 @@ public class Main extends SimpleApplication {
     @Override
     public void simpleInitApp() {
         //Set up the lighting, fog and position the camera
-        Environment env = new Environment(assetManager, rootNode, viewPort, cam);
+        Environment env = new Environment(assetManager, rootNode, viewPort);
 
-        //Set up the world
-            //Set up road
-            Road road = new Road(assetManager, rootNode);
+        
+        Road road = new Road(assetManager, rootNode);
             
-            //set up buildings
-            buildings = new Buildings(assetManager, rootNode);
+        //set up buildings
+        buildings = new Buildings(assetManager, rootNode);
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        //TODO: add update code
+        buildings.checkOutOfBounds();
     }
+    
+    
+    
 
-    @Override
-    public void simpleRender(RenderManager rm) {
-        buildings.tickPosition();
-    }
+   
 }
